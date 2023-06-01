9b0e1f962d80965531a070719409d649f61a066e
julia medvedeva <medvedevajulev@gmail.com>
2023-06-01 09:24:49 +0300

    clear up

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ function App() {
       <div className="Group">
         <a
           href="https://docs.google.com/document/d/1JwJSdh0hHmNqkkzdq3hUodH7hIO0Wu1GxkAviwa0yHo/edit?usp=sharing"
-          target="_blank">
+          target="_blank"
+          rel="noreferrer">
           <div className="Item">История первая</div>
         </a>
         <a
-          href="https://docs.google.com/document/d/1JwJSdh0hHmNqkkzdq3hUodH7hIO0Wu1GxkAviwa0yHo/edit?usp=sharing"
-          target="_blank">
+          href="https://docs.google.com/document/d/1xrbiAvN09xNaSN8tXobj2RoXp3xZlNvIZcbOqpwok9I/edit?usp=sharing"
+          target="_blank"
+          rel="noreferrer">
           <div className="Item">История вторая</div>
         </a>
+        <a
+          href="https://docs.google.com/document/d/1TWMK90aszpWCqwFc2U8Y2RQTCQl3__IR17GZmxMYC7Y/edit?usp=sharing"
+          target="_blank"
+          rel="noreferrer">
+          <div className="Item Large">История третья</div>
+        </a>
       </div>
-      <a
-        href="https://docs.google.com/document/d/1JwJSdh0hHmNqkkzdq3hUodH7hIO0Wu1GxkAviwa0yHo/edit?usp=sharing"
-        target="_blank">
-        <div className="Item Large">Не история</div>
-      </a>
     </div>
   );
 }
